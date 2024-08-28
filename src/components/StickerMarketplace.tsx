57b864dbf6ca86c4ddb26d0a0907f297c5ea3d5d
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-08-28 21:17:34 +0530

    nft mint

--- src/components/StickerMarketplace.tsx
+++ src/components/StickerMarketplace.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { Account,Ed25519PrivateKey, AccountAddress, Aptos, AptosConfig, Network, NetworkToNetworkName } from "@aptos-labs/ts-sdk";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 interface NFTPack {
   title: string;
@@ -12,36 +12,35 @@ interface NFTPack {
   altText: string;
 }
 
-const config = new AptosConfig({ network: Network.DEVNET });
-    const aptos = new Aptos(config);
+const StickerMarketplace: React.FC = () => {
 
-async function transferCoin(
-  sender: Account,
-  amount: any,
-): Promise<string> {
-  const transaction = await aptos.transaction.build.simple({
-    sender: sender.accountAddress,
-    data: {
-      function: "0x1::aptos_account::transfer_coins",
-      typeArguments: [`${sender.accountAddress}::telegage_token::TeleGageToken`],
-      functionArguments: [AccountAddress.fromString("0xf1e9e56bb36fb6b14a0e43bdc08dd13d5712eca1935c63870d1f3cc9827aab51"), amount],
-    },
-  });
-
-  const senderAuthenticator = aptos.transaction.sign({ signer: sender, transaction });
-  const pendingTxn = await aptos.transaction.submit.simple({ transaction, senderAuthenticator });
-
-  return pendingTxn.hash;
-}
-
-const StickerMarketplace: React.FC = ({privAdres}) => {
-  const privateKey =new Ed25519PrivateKey(privAdres)
-  const account = Account.fromPrivateKey({ privateKey: privateKey });
+  const { account } = useWallet();
+ 
   const [nftPacks, setNFTPacks] = useState<NFTPack[]>([]);
 
-  
+  const mint_nftpack = async (pack: NFTPack) => {
+    const data = {
+      action: "Add Sticker",
+      prompt: pack.keywords,
+      wallet: account.address.toString(),
+      negative_prompt: pack.negative,
+      price: pack.price
+    };
+
+    console.log(data)
+    
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.sendData(JSON.stringify(data));
+    } else {
+      console.log("Telegram WebApp not available. Data:", data);
+    }
+  };
 
   useEffect(() => {
+    if (window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+    }
+    
     const fetchNFTPacks = async () => {
       try {
 
@@ -55,26 +54,6 @@ const StickerMarketplace: React.FC = ({privAdres}) => {
 
     fetchNFTPacks();
   }, []);
-  const getBalance = async (accountAddress: AccountAddress, coinTypeAddress: AccountAddress) => {
-    const amount = await aptos.getAccountCoinAmount({
-      accountAddress,
-      coinType: `${coinTypeAddress.toString()}::telegage_token::TeleGageToken`,
-    });
-  
-    return amount;
-  };
-  const transferToken = async () => {
-    await aptos.fundAccount({
-      accountAddress: account.accountAddress,
-      amount: 100_000_000,
-  }); 
-    const pendingTxn = await transferCoin(account, 1);
-    console.log("Pending transaction:", pendingTxn);
-
-    const transferCoinTransactionHash = await transferCoin(account, 100);
-    await aptos.waitForTransaction({ transactionHash: transferCoinTransactionHash });
-    console.log(`Bob's updated MoonCoin balance: ${await getBalance( account.accountAddress, account.accountAddress)}.`);
-  };
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
@@ -99,7 +78,7 @@ const StickerMarketplace: React.FC = ({privAdres}) => {
           <h3 className="text-lg font-semibold mb-1 text-white">{pack.title}</h3>
           <div className="flex justify-between items-center">
             <span className="text-gray-300">{pack.price} Tokens</span>
-            <button className="bg-purple-500 text-white py-1 px-3 rounded hover:bg-purple-600 transition-colors duration-200" onClick={transferToken}>Buy</button>
+            <button className="bg-purple-500 text-white py-1 px-3 rounded hover:bg-purple-600 transition-colors duration-200" onClick={mint_nftpack}>Buy</button>
           </div>
         </motion.div>
       ))}
